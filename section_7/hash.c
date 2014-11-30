a781cc0b76da027cacca43bd784cfce106492890
Yitz Lefkowitz <yitzlefkowitz@gmail.com>
2014-11-30 17:32:52 -0500

    Add hash function based on djb2

--- section_7/hash.c
+++ section_7/hash.c
@@ -2,14 +2,44 @@
 #include <stdbool.h>
 
 /* Prototypes */
-int hash (string key);
+unsigned long hash (char* key);
+
+/* Constants */
+#define NUM_STRINGS 10
 
 int main (void) 
 {
+	char* strings[NUM_STRINGS] = 
+	{
+		"aaa",
+		"aab",
+		"hello",
+		"cat",
+		"dog",
+		"kenny",
+		"Kenny",
+		"double",
+		"ties",
+		"egypt"
+	};
+	
+	long hash_return;
+	
+	for (int i=0; i<NUM_STRINGS; i++)
+	{
+		hash_return = hash(strings[i]);
+		printf("%s has a hash value of %li \n", strings[i], hash_return);
+	}
 	
 }
 
-int hash (string key)
+unsigned long hash (char* key)
 {
-	
+	unsigned long hash = 5381;
+    int c;
+
+    while ((c = *key++) != 0)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return (hash<0) ? hash*-1 : hash;
 }
