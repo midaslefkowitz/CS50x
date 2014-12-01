42f6432f8a612c4750138bb59b4b48c43362e4f4
Yitz Lefkowitz <yitzlefkowitz@gmail.com>
2014-12-01 16:44:52 -0500

    finish hash load and find

--- section_7/hash.c
+++ section_7/hash.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include <cs50.h>
 
 // typedef a node for the linked list
 typedef struct node
@@ -12,20 +13,51 @@ typedef struct node
 
 /* Prototypes */
 unsigned long hash (char* key);
-bool insert (char* value, node* head);
+bool insert (char* value, int index);
 bool load_table();
 char* toLower (char* word);
+bool find (char* word_to_find);
 
 /* Constants */
 #define NUM_STRINGS 10
+
+/* Globals */
+node* hash_table[NUM_STRINGS];
 
 int main (void) 
 {
 	if (!load_table())
 	{
 		// table didn't load properly
+		printf("table didn't load properly\n");
 		return 1;
 	}
+	
+	while (true)
+	{
+		printf("1. Find a word in the dictionary\n");
+		printf("2. Quit\n");
+
+		int user_choice;
+		do 
+		{
+			user_choice = GetInt();
+		} while (user_choice != 1 && user_choice != 2);
+		
+		switch (user_choice)
+		{
+			case 1:
+				printf("Please enter a word: ");
+                char* word_to_find = GetString();
+                char* success = find(word_to_find) ? "is" : "is not";
+                printf("%s %s in the dictionary.\n",word_to_find, success);
+                break;
+            case 2:
+            	return 0;
+            	break;
+		}
+	}
+	return 0;
 }
 
 bool load_table()
@@ -44,9 +76,6 @@ bool load_table()
 		"egypt"
 	};
 	
-	// declare hash table as array of linked lists
-	node* hash_table[NUM_STRINGS];
-	
 	// initialize the cells in the table to NULL
 	for (int i=0; i<NUM_STRINGS; i++)
 	{
@@ -56,7 +85,7 @@ bool load_table()
 	// for each word in the dummy data list
 	for (int i=0; i<NUM_STRINGS; i++)
 	{
-		// convert the key to all lowercase
+		// convert the key o all lowercase
 		char* key = toLower(strings[i]);
 		
 		// get the hash value of the string
@@ -66,7 +95,7 @@ bool load_table()
 		hash_value = hash_value % NUM_STRINGS;
 		
 		// insert the value into the hash table
-		if (!insert(strings[i], hash_table[hash_value]))
+		if (!insert(key, hash_value))
 		{
 			return false;
 		}
@@ -106,7 +135,7 @@ unsigned long hash (char* key)
     return hash;
 }
 
-bool insert (char* value, node* head)
+bool insert (char* value, int index)
 {
 	// allocate and initialize new node
 	node* new_node = malloc(sizeof(node));
@@ -122,34 +151,58 @@ bool insert (char* value, node* head)
 	new_node->next = NULL;
 	
 	// first insertion
-    if (head == NULL)
+    if (hash_table[index] == NULL)
     {
         new_node->next = NULL;
-        head = new_node;
+        hash_table[index] = new_node;
         return true;
     }
     
     else 
     {
     	// check if string already in table
-    	node* temp = head;
+    	node* temp = hash_table[index];
     	while (temp != NULL)
     	{
-			if (strcmp(value, temp->word))
+			if ((strcmp(value, temp->word)) == 0)
 		    {
 		        free(new_node);
-		        return false;
+		        return true;
 		    }
 		    temp = temp->next;
         }
         /* insert word */
         // save reference to next node
-        temp = head->next;
+        temp = hash_table[index]->next;
         // insert node at beginning of list
-		head = new_node;
+		hash_table[index] = new_node;
 		// update the reference to next node
-		head->next = temp;
+		hash_table[index]->next = temp;
 		return true;
 	}
 	return true;
+}
+
+bool find (char* word_to_find)
+{
+	// get the hash value of the word_to_find
+	unsigned long hash_value = hash(word_to_find);
+	// modulo the hash value to size of table
+	hash_value = hash_value % NUM_STRINGS;
+	
+	// go to the corresponding linked list in the hash_table
+	node* current = hash_table[hash_value];
+	// traverse the list looking for word_to_find
+	while (current != NULL)
+	{
+		// if the node word == word_to_find
+		if ((strcmp(current->word, word_to_find)) == 0)
+		{
+			// return true
+			return true;
+		}
+		current = current->next;
+	}
+	// traversed and didn't find. return false	
+	return false;
 }
