e63b26eb6b4fea8f33b72c235895f6449c56b13f
Yitz Lefkowitz <yitzlefkowitz@gmail.com>
2014-12-04 09:57:44 -0500

    More work on dict load

--- pset6/dictionary.c
+++ pset6/dictionary.c
@@ -11,6 +11,46 @@
 
 #include "dictionary.h"
 
+/* Node in the trie */
+typedef struct node
+{
+    bool word_end;
+    struct node* children[27];
+} node;
+
+/* Globals */
+int num_words = 0;
+node* root = malloc(sizeof(node));
+
+/**
+ * Returns true if the char is valid for trie
+ */
+bool isValid(char letter)
+{
+	if (letter < 'A')
+	{
+		// it might be ' or newline		
+		return (letter == '\n' || letter == '\'');
+	}
+	if (letter > 'Z')
+	{
+		// check if it's a lowercase letter
+		return (letter >= 'a' && letter <= 'z');
+	}
+}
+
+/**
+ * If char is uppercase converts lowercase otherwise retains state
+ */
+ char toLower(char letter)
+ {
+ 	if (letter >= 'A' && letter <= 'Z')
+ 	{
+ 		letter += ('a' - 'A');
+ 	}
+ 	return letter;
+ }
+
 /**
  * Returns true if word is in dictionary else false.
  */
@@ -25,7 +65,45 @@ bool check(const char* word)
  */
 bool load(const char* dictionary)
 {
-    // TODO
+	// Open dictionary
+	FILE* file = fopen(dictionary, "r");
+	
+	// Ensure that found the dictionary
+	if (file == NULL)
+	{
+		printf("Couldn't find dictionary!\n");
+		return false;
+	}
+		
+	// temporary storage 
+	char buffer;
+	
+	// current position in trie
+	node* current = root;
+	
+	// iterate through the dictionary one char at a time
+	while (fread(buffer, sizeof(char), 1, file))
+	{
+		// ensure that that the char is valid
+		if (isValid(buffer))
+		{
+			// convert uppercase to lowercase
+			buffer = toLower(buffer);
+			
+			// if dict char == /n 
+			
+				// mark current node as word_end
+			
+				// increment total words
+			
+				// reset current to root
+			
+			// else 
+				
+				// 
+	
+		}
+	}
     return false;
 }
 
